3fb85a99f726a5bc480e80eb8d7ca6b166a73f5d
tlpackus <tlpackus@gmail.com>
2021-04-20 21:02:24 -0700

    ask player for input then give input as argument for Game Co-authored-by: Shanen Cross <shanencross@yahoo.com> Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
+using RockPaperScissors.Models;
 
 namespace RockPaperScissors
 {
+
   public class Program
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Choose rock, scissors or paper");
+      string playerAnswer = System.Console.ReadLine();
+      Game newGame = new Game(playerAnswer);
     }
   }
 }
