34c9457337d24695d8aee6c9ff15481ecb143ec2
Shanen Cross <shanencross@yahoo.com>
2021-04-21 20:48:46 -0700

    Finish basic terminal interface
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -4,14 +4,25 @@ using RockPaperScissors.Models;
 
 namespace RockPaperScissors
 {
-
   public class Program
   {
     public static void Main()
     {
-      Console.WriteLine("Choose rock, scissors or paper");
-      string playerAnswer = System.Console.ReadLine();
-      Game newGame = new Game(playerAnswer);
+      bool runGame = true;
+
+      while (runGame)
+      {
+        Console.WriteLine("Choose rock, scissors or paper");
+        string playerAnswer = System.Console.ReadLine();
+        Game newGame = new Game(playerAnswer);
+
+        Console.WriteLine("The computer threw " + newGame.ComputerThrow + "!");
+        Console.WriteLine(newGame.GetWinMessage());
+
+        if (newGame.Winner != "Draw") {
+          runGame = false;
+        }
+      }
     }
   }
 }
